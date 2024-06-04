b3767033eb6839c402b5bc5b738b96d27d0778f5
samiunique <yitesam@gmail.com>
2024-06-04 20:12:08 +0300

    eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
 export default function getListStudents() {
-	const o1 = {
-		id: 1,
-		firstName: 'Guillaume',
-		location: 'San Francisco',
-	};
+	const students = [
+		{
+			id: 1,
+			firstName: 'Guillaume',
+			location: 'San Francisco',
+		},
+		{
+			id: 2,
+			firstName: 'James',
+			location: 'Columbia',
+		},
+		{
+			id: 5,
+			firstName: 'Serena',
+			location: 'San Francisco',
+		}
+	];
 
-	const o2 = {
-		id: 2,
-		firstName: 'James',
-		location: 'Columbia',
-	};
-
-	const o3 = {
-		id: 5,
-		firstName: 'Serena',
-		location: 'San Francisco',
-	};
-
-	return [o1, o2, o3];
+	return students;
 }
