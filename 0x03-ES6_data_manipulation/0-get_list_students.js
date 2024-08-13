6809b0e4d8b0da76bfc37a48e78624c241226174
samiunique <yitesam@gmail.com>
2024-08-13 20:12:35 +0300

    indentation fixed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,16 @@
 export default function getListStudents() {
-	const students = [
-		{
-			id: 1,
-			firstName: 'Guillaume',
-			location: 'San Francisco',
-		},
-		{
-			id: 2,
-			firstName: 'James',
-			location: 'Columbia',
-		},
-		{
+  const students = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+	},
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+	},
+    {
 			id: 5,
 			firstName: 'Serena',
 			location: 'San Francisco',
